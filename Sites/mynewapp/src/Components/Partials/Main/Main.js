2628fbdee7d1501c24605cd8970675f23dfb02f3
Heinz K <heka@techcollege.dk>
2022-01-12 11:00:22 +0100

    Image Guide Update

--- Sites/mynewapp/src/Components/Partials/Main/Main.js
+++ Sites/mynewapp/src/Components/Partials/Main/Main.js
@@ -1,7 +1,11 @@
+import { ReactComponent as Logo } from '../../../Assets/images/logo.svg'
+
 export const Main = () => {
 	return (
 	  <main>
 		  <h2>My First React App</h2>
+		  <p><img alt="REACT Logo" src={require('../../../Assets/images/logo.png')} /></p>
+		  <Logo />
 	  </main>
 	)
   }
