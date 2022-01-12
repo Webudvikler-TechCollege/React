31562765c4882bcaa14d7dbd1d1d924da0b88e51
Heinz K <heka@techcollege.dk>
2022-01-12 12:03:25 +0100

    CSS Guides Update

--- Sites/myapp/src/App.js
+++ Sites/myapp/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import { Header } from './components/partials/Header/Header'
 import { Main } from './components/partials/Main/Main'
 import { Footer } from './components/partials/Footer/Footer'
