0d96e6bac1b7a3acaae95d339321a9e6d599e5ae
Heinz K <heka@techcollege.dk>
2022-01-18 08:50:40 +0100

    Router Update

--- Sites/codealong_app/src/App.js
+++ Sites/codealong_app/src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Header />
         <Main>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route index element={<Home />} />
             <Route path="/about" element={<About />} />
           </Routes>
         </Main>
