0325bbea1c8f5b4141d2963e386374a470c89c8a
Heinz K <heka@techcollege.dk>
2022-01-28 12:05:22 +0100

    Login Update

--- Sites/codealong/src/Assets/data/arrNavItems.js
+++ Sites/codealong/src/Assets/data/arrNavItems.js
@@ -1,6 +1,7 @@
 import { Home } from '../../Components/Pages/Home/Home'
 import { About } from '../../Components/Pages/About/About'
 import { GoalList } from '../../Components/Pages/Goals/GoalList'
+import { Login } from '../../Components/Pages/Login/Login'
 
 const arrNavItems = [
 	{
@@ -20,6 +21,12 @@ const arrNavItems = [
 		path: '/goals',
 		element: <GoalList />,
 		display: false
+	},
+	{
+		title: 'Login',
+		path: '/login',
+		element: <Login />,
+		display: true
 	}
 ]
 
---
+++ Sites/codealong/src/Components/Pages/Login/Login.jsx
@@ -0,0 +1,61 @@
+import axios from 'axios'
+import { useState } from 'react'
+
+export const Login = () => {
+	const txMessage = "Indtast brugernavn og adgangskode for at logge ind"
+	const [message, setMessage] = useState(txMessage);
+	const [loginData, setLoginData] = useState('')
+
+	const SubmitLoginForm = async e => {
+		const formData = new FormData(e.target.form)
+		try {
+			const result = await axios.post('https://api.mediehuset.net/token', formData)
+			handleSessionData(result);	
+		}
+		catch(err) {
+			console.error(err)
+			setMessage('Kunne ikke logge ind')
+		}
+	}
+
+	const handleSessionData = resp => {
+		if(resp.status === 201) {			
+			sessionStorage.setItem('token', JSON.stringify(resp.data))
+			setLoginData(resp.data)
+			setMessage(`Du er logget ind som ${resp.data.username}`)
+		}
+	}
+
+	const logOut = () => {
+		sessionStorage.removeItem('token')
+		setLoginData('')
+		setMessage(txMessage)
+	}
+
+	return (
+		<div>
+			<h1>Login</h1>
+			<p>{message}</p>
+			{!loginData ? 
+				<form method="post">
+					<div>
+						<label htmlFor="username">Brugernavn: </label>
+						<input type="text" name="username" id="username" />
+					</div>
+					<div>
+						<label htmlFor="password">Adgangskode: </label>
+						<input type="password" name="password" id="password" />
+					</div>
+					<div>
+						<button type="button" onClick={SubmitLoginForm}>Login</button>
+						<button type="reset">Nulstil felter</button>
+					</div>
+				</form>
+				:
+				<form>
+					<button type="button" onClick={logOut}>Log out</button>
+				</form>
+			}
+		</div>
+	);
+}
