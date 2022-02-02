dbae7114be079174a414a44a1a2b8c87f2183c73
Heinz K <heka@techcollege.dk>
2022-02-02 12:01:30 +0100

    Product Routing

--- Sites/deployment/src/Components/AppRouter/AppRouter.jsx
+++ Sites/deployment/src/Components/AppRouter/AppRouter.jsx
@@ -1,16 +1,28 @@
 import { Routes, Route, useNavigate, Outlet } from 'react-router-dom'
 import { Home } from '../Pages/Home/Home'
 import { About } from '../Pages/About/About'
-import { Products } from '../Pages/Products/Products'
+import { CategoryList, ProductDetails, ProductList, Products } from '../Pages/Products/Products'
 import { useEffect } from 'react'
+
+const Redirect = ({ to }) => {
+	let navigate = useNavigate();
+	useEffect(() => {
+		navigate(to);
+	});
+	return null;
+}
 
 export const AppRouter = () => {
 	return (
 		<Routes>
 			<Route index element={<Home />} />
 			<Route path="/about" element={<About />} />
-			<Route path="/products">
-				<Outlet />
+			<Route path="/products" element={<CategoryList />}>
+				<Route index element={<Redirect to="/products/1" />}></Route>
+				<Route path=":cat_id">
+					<Route index element={<ProductList />} />
+					<Route path=":id" element={<ProductDetails />} />
+				</Route>
 			</Route>
 		</Routes>
 	)
--- Sites/deployment/src/Components/Pages/Products/Products.jsx
+++ Sites/deployment/src/Components/Pages/Products/Products.jsx
@@ -1,25 +1,13 @@
 import { useEffect, useState } from "react"
 import axios from 'axios'
-import { Route, Link, useParams, useNavigate } from "react-router-dom"
+import { Route, Link, useParams, useNavigate, Outlet } from "react-router-dom"
 
-const Redirect = ({ to }) => {
-	let navigate = useNavigate();
-	useEffect(() => {
-		navigate(to);
-	});
-	return null;
-}
+
 
 export const Products = () => {
 	return (
 		<div className="container">
-			<CategoryList />
-			<Route index element={<Redirect to="/products/1" />}></Route>
-				<Route path=":cat_id">
-					<Route index element={<Products />} />
-					<Route path=":id" element={<Products />} />
-			</Route>
-			<ProductList />
+			<Outlet />
 		</div>
 	)
 }
@@ -74,7 +62,7 @@ export const ProductList = () => {
 	)
 }
 
-const ProductDetails = () => {
+export const ProductDetails = () => {
 	const [product, setProduct] = useState({})
 	const {id} = useParams()
 
