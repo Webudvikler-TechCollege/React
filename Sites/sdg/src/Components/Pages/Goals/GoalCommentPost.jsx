173260e17db68ee2aecd446d4f45346a97102ccd
Heinz K <heka@techcollege.dk>
2022-08-22 07:39:13 +0200

    Goal Post Eksempel

--- Sites/sdg/src/Components/Pages/Goals/GoalCommentPost.jsx
+++ Sites/sdg/src/Components/Pages/Goals/GoalCommentPost.jsx
@@ -2,11 +2,7 @@ import { useForm } from "react-hook-form";
 import { AppService } from "../../App/AppServices/AppService";
 
 export const GoalCommentPost = (props) => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+  const { register, handleSubmit, formState: { errors }} = useForm();
 
   const onSubmit = async (data) => {
     const formData = new FormData();
@@ -23,17 +19,25 @@ export const GoalCommentPost = (props) => {
   return (
     <div><hr />
       <form onSubmit={handleSubmit(onSubmit)}>
+        <fieldset>
         <div>
           <label htmlFor="title">Titel</label><br />
           <input type="text" id="title" placeholder="Indtast en titel" {...register('title', {required: true, maxLength: 200})}/>
+          {errors.title && (
+              <span>Du skal skrive en titel!</span>
+          )}
         </div>
         <div>
           <label htmlFor="comment">Kommentar</label><br />
           <textarea id="comment" {...register('comment', {required: true})}></textarea>
+          {errors.comment && (
+              <span>Du skal skrive en kommentar!</span>
+          )}
         </div>
         <div>
           <button>Send</button>
         </div>
+        </fieldset>
       </form>
     </div>
   );
