4f3324020074ea7082b8edab258b6c183048e7b7
Heinz K <heka@techcollege.dk>
2023-01-30 09:40:57 +0100

    SC Global Style

--- Sites Spring 2023/codealong/wallywood/src/App.jsx
+++ Sites Spring 2023/codealong/wallywood/src/App.jsx
@@ -1,10 +1,14 @@
 import { ContainerStyle } from './Components/Styled/Container.style'
+import { GlobalStyle } from './Components/Styled/Global.style';
 
 function App() {
   return (
+    <>
+    <GlobalStyle />
     <ContainerStyle bg="blue">
       sdfdfdf
     </ContainerStyle>
+    </>
   );
 }
 
---
+++ Sites Spring 2023/codealong/wallywood/src/Components/Styled/Global.style.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+	* {
+		box-sizing: border-box;
+	}
+
+	body {
+		background-color: aliceblue;
+	}
+`
+
+export { GlobalStyle }
